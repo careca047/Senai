0573b8b2c05862f88eb7c3d5734362ba054d802a
careca047 <kaina.vieira@hotmail.com>
2024-11-15 16:09:43 -0300

    alterações e correçoes majoritarias nas paginas de produto e lista de produtos, pequenas mudanças na pagina de cadastro porem ainda em andamento

--- Game-Mania_2.0/theme/assets/corporate/scripts/layout.js
+++ Game-Mania_2.0/theme/assets/corporate/scripts/layout.js
@@ -456,16 +456,16 @@ var Layout = function () {
 
         initSliderRange: function () {
             $( "#slider-range" ).slider({
-              range: true,
-              min: 0,
-              max: 500,
-              values: [ 50, 250 ],
-              slide: function( event, ui ) {
-                $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
-              }
+            range: true,
+            min: 0,
+            max: 5000,
+            values: [ 50, 1000 ],
+            slide: function( event, ui ) {
+                $( "#amount" ).val( "R$" + ui.values[ 0 ] + " - R$" + ui.values[ 1 ] );
+            }
             });
-            $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
-            " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+            $( "#amount" ).val( "R$" + $( "#slider-range" ).slider( "values", 0 ) +
+            " - R$" + $( "#slider-range" ).slider( "values", 1 ) );
         },
 
         // wrapper function to scroll(focus) to an element
